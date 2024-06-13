718b194912cbe4b50e9dea198b2edd8651ee1ed0
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-06-13 10:17:08 -0300

    feat: add components for landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent, NavbarComponent } from './components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'cafeteria';
-}
+export class AppComponent {}

f0b7cca8297c37b9c73d4027a1ee699782e0734b
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-06-13 10:16:38 -0300

    feat: add packages and configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
@@ -6,6 +8,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [addDynamicIconSelectors()]
 }
 
